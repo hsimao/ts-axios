22bb7a91d13d3a43ff0e7e906d1b83447ae5e308
hsimao <e087754958@gmail.com>
2019-05-11 23:10:21 +0800

    feat(src): create basis http request

---
+++ src/types/index.ts
@@ -0,0 +1,23 @@
+// 定義 method 參數可接收的字串
+export type Method =
+  | 'get'
+  | 'GET'
+  | 'delete'
+  | 'DELETE'
+  | 'head'
+  | 'HEAD'
+  | 'options'
+  | 'OPTIONS'
+  | 'post'
+  | 'POST'
+  | 'put'
+  | 'PUT'
+  | 'patch'
+  | 'PATCH'
+
+export interface AxiosRequestConfig {
+  url: string
+  method?: Method
+  data?: any
+  params?: any
+}
